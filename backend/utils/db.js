200b104da0b67968220f4645c792c9195d4e5012
Ayush Kumar <pinku@Ayushs-MacBook-Air.local>
2026-04-28 06:36:02 +0530

    Updated backend API and frontend UI

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
+import dns from "dns";
 
-import dns from 'dns';
-dns.setServers(['8.8.8.8'])
-
-let DB_URL = process.env.MONGO_URI
+dns.setServers(["8.8.8.8"]);
 
 async function connectToDB() {
-    try {
-        await mongoose.connect(DB_URL)
-        console.log("MongoDB Connected")
-    } catch (error) {
-        console.log("Database Error")
-        console.log(error)
+  try {
+    const DB_URL = process.env.MONGO_URI; 
+
+    if (!DB_URL) {
+      throw new Error("MONGO_URI is undefined");
     }
+
+    await mongoose.connect(DB_URL);
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("Database Error");
+    console.log(error.message);
+  }
 }
 
-export default connectToDB
+export default connectToDB;
